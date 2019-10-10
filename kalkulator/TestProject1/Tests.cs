24965b9cb6ffc4baee053155c94fcdbb7ed32429
horseburger <pralka.gniewu@gmail.com>
2019-10-10 17:05:31 +0200

    feat: add unit tests

---
+++ kalkulator/TestProject1/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using kalkulator;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(new Class1().Dodaj(2, 5) == 7);
+        }
+    }
+}
