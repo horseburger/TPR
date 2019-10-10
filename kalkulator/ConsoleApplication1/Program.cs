f72a41d0cffab705c80d1dc32c7b8f4c411feed3
horseburger <pralka.gniewu@gmail.com>
2019-10-10 16:52:09 +0200

    feat: add main to the test project

---
+++ kalkulator/ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using kalkulator;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            Console.WriteLine(c.Dodaj(2, 5));
+        }
+    }
+}
--- kalkulator/kalkulator/Class1.cs
+++ kalkulator/kalkulator/Class1.cs
@@ -2,5 +2,8 @@
 {
     public class Class1
     {
+        public float Dodaj(float a, float b)
+        {
+            return a + b;}
     }
 }
