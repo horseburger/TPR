2b663dd5111c1309bc38cc7e00e1949cd1c37534
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-10-15 21:34:28 +0200

    add: tests for DataRepository

--- t1/part_three_test/Test.cs
+++ t1/part_three_test/Test.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using part_one;
 using part_two;
 
@@ -13,6 +14,33 @@ namespace part_three_test
             DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
             dataRepository.AddKatalog(new Katalog(1, "example"));
             Assert.AreEqual(1, dataRepository.GetAllKatalog().Count);
+        }
+        [Test]
+        public void GetKatalogTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Katalog katalog = new Katalog(1, "example");
+            dataRepository.AddKatalog(katalog);
+            Assert.AreEqual(katalog, dataRepository.GetKatalog(1));
+        }
+        [Test]
+        public void GetAllKatalogTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Dictionary<int, Katalog> dictionary = dataRepository.GetAllKatalog();
+            Katalog katalog = new Katalog(1, "example");
+            dataRepository.AddKatalog(katalog);
+            Assert.AreEqual(katalog, dataRepository.GetAllKatalog());
+        }
+        [Test]
+        public void UpdateKatalogTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Katalog katalog = new Katalog(1, "example");
+            dataRepository.AddKatalog(katalog);
+            Katalog katalog2 = new Katalog(1, "hello");
+            dataRepository.UpdateKatalog(1, katalog2);
+            Assert.AreEqual(katalog2, dataRepository.GetKatalog(1));
         }
     }
 }
