e19287749ca2349b8cc84cca8380918b78a965f9
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-10-15 19:25:30 +0200

    add: CRUD declaration in DataRepository (not implemented)

--- t1/part_one/DataRepository.cs
+++ t1/part_one/DataRepository.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace part_one
 {
     public class DataRepository
@@ -10,6 +12,46 @@ namespace part_one
         public DataRepository(DataFiller api)
         {
             this.api = api;
+        }
+        public void AddKatalog(Katalog pozycja)
+        {
+
+        }
+        public Katalog GetKatalog(int id)
+        {
+            return null;
+        }
+        public Dictionary<int, Katalog> GetAllKatalog()
+        {
+            return null;
+        }
+        public void UpdateKatalog(int id, Katalog pozycja)
+        {
+
+        }
+        public void DeleteKatalog(Katalog pozycja)
+        {
+
+        }
+        public void AddWykaz(Wykaz element)
+        {
+
+        }
+        public Wykaz GetWykaz(int id)
+        {
+            return null;
+        }
+        public List<Wykaz> GetAllWykaz()
+        {
+            return null;
+        }
+        public void UpdateWykaz(int id, Wykaz element)
+        {
+
+        }
+        public void DeleteWykaz(Wykaz element)
+        {
+
         }
     }
 }
