a366a58541454ed62707971be5d1a4f85c050c6f
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-10-15 21:46:10 +0200

    fix:

--- t1/part_three_test/Test.cs
+++ t1/part_three_test/Test.cs
@@ -30,7 +30,7 @@ namespace part_three_test
             Dictionary<int, Katalog> dictionary = dataRepository.GetAllKatalog();
             Katalog katalog = new Katalog(1, "example");
             dataRepository.AddKatalog(katalog);
-            Assert.AreEqual(katalog, dataRepository.GetAllKatalog());
+            Assert.AreEqual(dictionary, dataRepository.GetAllKatalog());
         }
         [Test]
         public void UpdateKatalogTest()
@@ -41,6 +41,58 @@ namespace part_three_test
             Katalog katalog2 = new Katalog(1, "hello");
             dataRepository.UpdateKatalog(1, katalog2);
             Assert.AreEqual(katalog2, dataRepository.GetKatalog(1));
+        }
+        [Test]
+        public void DeleteKatalogTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Katalog katalog = new Katalog(1, "example");
+            dataRepository.AddKatalog(katalog);
+            dataRepository.DeleteKatalog(katalog);
+            Assert.AreEqual(0, dataRepository.GetAllKatalog().Count);
+        }
+        [Test]
+        public void AddWykazTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            dataRepository.AddWykaz(new Wykaz("Jan", "Kowalski"));
+            Assert.AreEqual(1, dataRepository.GetAllWykaz().Count);
+        }
+        [Test]
+        public void GetWykazTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Wykaz wykaz = new Wykaz("Jan", "Kowalski");
+            dataRepository.AddWykaz(wykaz);
+            Assert.AreEqual(wykaz, dataRepository.GetWykaz(0));
+        }
+        [Test]
+        public void GetAllWykazTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            List<Wykaz> lista = dataRepository.GetAllWykaz();
+            Wykaz wykaz = new Wykaz("Jan", "Kowalski");
+            dataRepository.AddWykaz(wykaz);
+            Assert.AreEqual(lista, dataRepository.GetAllWykaz());
+        }
+        [Test]
+        public void UpdateWykazTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Wykaz wykaz = new Wykaz("Jan", "Kowalski");
+            dataRepository.AddWykaz(wykaz);
+            Wykaz wykaz2 = new Wykaz("Adrian", "Nowak");
+            dataRepository.UpdateWykaz(0, wykaz2);
+            Assert.AreEqual(wykaz2, dataRepository.GetWykaz(0));
+        }
+        [Test]
+        public void DeleteWykazTest()
+        {
+            DataRepository dataRepository = new DataRepository(new WypelnianieDanymi());
+            Wykaz wykaz = new Wykaz("Jan", "Kowalski");
+            dataRepository.AddWykaz(wykaz);
+            dataRepository.DeleteWykaz(wykaz);
+            Assert.AreEqual(0, dataRepository.GetAllWykaz().Count);
         }
     }
 }
