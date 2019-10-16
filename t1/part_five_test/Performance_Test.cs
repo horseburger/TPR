7920cbe31414b956b3a782af20c84a87e4459be8
horseburger <pralka.gniewu@gmail.com>
2019-10-16 14:45:32 +0200

    fix: move Performance_Test to the part_five_test namespace

--- t1/part_five_test/Performance_Test.cs
+++ t1/part_five_test/Performance_Test.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Diagnostics;
 using NUnit.Framework;
+using part_five;
 using part_one;
 
-namespace part_five
+namespace part_five_test
 {
     [TestFixture]
     public class Performance_Test
@@ -20,14 +21,14 @@ namespace part_five
         public void TenThousandEntries_Test()
         {
             DataRepository repo = new DataRepository(new DataFillerRandom(10000));
-            Assert.That(Time((() => repo.Api.Fill(repo.storage))), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(20)));
+            Assert.That(Time((() => repo.Api.Fill(repo.Storage))), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(20)));
         }
 
         [Test]
         public void OneThousandEntries_Test()
         {
             DataRepository repo = new DataRepository(new DataFillerRandom(1000));
-            Assert.That(Time(() => repo.Api.Fill(repo.storage)), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(15)));
+            Assert.That(Time(() => repo.Api.Fill(repo.Storage)), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(15)));
         }
         
         
