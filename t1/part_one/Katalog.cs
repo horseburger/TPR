502ec2a7b18148ca9473595067a9b22717f8a231
horseburger <pralka.gniewu@gmail.com>
2019-10-16 11:43:08 +0200

    feat: start work on part five

--- t1/part_one/Katalog.cs
+++ t1/part_one/Katalog.cs
@@ -2,20 +2,20 @@ namespace part_one
 {
     public class Katalog
     {
-        private int id;
+        private int _id;
         private string info;
 
         public int Id
         {
-            get => id;
-            set => id = value;
+            get => _id;
+            set => _id = value;
         }
         public string Info => info;
 
         public Katalog(int id, string info)
         {
             this.info = info;
-            this.id = id;
+            this._id = id;
         }
     }
 }
