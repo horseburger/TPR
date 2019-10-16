e46d7f650b50837b10b6bc646ab11d067baf86c7
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-10-16 10:39:41 +0200

    add: part four starting project

---
+++ t1/part_four/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using part_one;
+using part_two;
+
+namespace part_four
+{
+    class DataService
+    {
+        private DataRepository repository;
+
+        public DataService(DataRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
+
