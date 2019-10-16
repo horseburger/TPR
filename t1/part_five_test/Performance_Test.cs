de103c3a47978feea6b4171e3a684c482e3d301f
horseburger <pralka.gniewu@gmail.com>
2019-10-16 14:42:11 +0200

    feat: add performance unit tests

---
+++ t1/part_five_test/Performance_Test.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using part_one;
+
+namespace part_five
+{
+    [TestFixture]
+    public class Performance_Test
+    {
+        private TimeSpan Time(Action toTime)
+        {
+            var timer = Stopwatch.StartNew();
+            toTime();
+            timer.Stop();
+            return timer.Elapsed;
+        }
+        
+        [Test]
+        public void TenThousandEntries_Test()
+        {
+            DataRepository repo = new DataRepository(new DataFillerRandom(10000));
+            Assert.That(Time((() => repo.Api.Fill(repo.storage))), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(20)));
+        }
+
+        [Test]
+        public void OneThousandEntries_Test()
+        {
+            DataRepository repo = new DataRepository(new DataFillerRandom(1000));
+            Assert.That(Time(() => repo.Api.Fill(repo.storage)), Is.LessThanOrEqualTo(TimeSpan.FromSeconds(15)));
+        }
+        
+        
+    }
+}
