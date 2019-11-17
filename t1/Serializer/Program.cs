0151aeb16c18ad0fb17cefbe4de80866f6df128b
horseburger <pralka.gniewu@gmail.com>
2019-11-17 16:58:04 +0100

    feat: add Program implementation serializing DataContext and later deserializing it

--- t1/Serializer/Program.cs
+++ t1/Serializer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using Bookstore;
@@ -9,7 +10,45 @@ namespace Serializer
 {
     internal class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            string filename = "file";
+            DataRepository repo = new DataRepository(new Filler());
+            repo.Api.Fill(repo.GetStorage());
+            Serializer serializer = new Serializer();
+            string json = serializer.SerializeItemJson(repo.GetStorage());
+            Console.WriteLine(json);
+            serializer.SerializeItemJson(filename + ".json", repo.GetStorage());
+            serializer.SerializeItemBinary(filename + ".data", repo.GetStorage());
+            DataContext c1 = serializer.DeserializeItemJson(json);
+            DataContext c2 = serializer.DeserializeItemJsonFromFile(filename + ".json");
+            DataContext c3 = serializer.DeserializeItemBinary(filename + ".data");
+
+            Console.WriteLine("Original repo and c1: " + CheckIfSame(repo.GetStorage(), c1));
+            Console.WriteLine("Original repo and c2: " + CheckIfSame(repo.GetStorage(), c2));
+            Console.WriteLine("Original repo and c3: " + CheckIfSame(repo.GetStorage(), c3));
+            
+        }
+
+        private static bool CheckIfSame(DataContext c1, DataContext c2)
+        {
+            if (c1.Clients.Count == c2.Clients.Count)
+            {
+                for (int i = 0; i < c1.Clients.Count; i++)
+                {
+                    if (!c1.Clients[i].Equals(c2.Clients[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 
     public class Filler : IDataFiller
