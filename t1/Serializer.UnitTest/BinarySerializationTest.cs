fa581a84da90cd4b88de5da9e282f0b70b2bda54
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-11-17 01:40:50 +0100

    add: binary serialization and deserialization tests

--- t1/Serializer.UnitTest/BinarySerializationTest.cs
+++ t1/Serializer.UnitTest/BinarySerializationTest.cs
@@ -1,10 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Bookstore;
+using Bookstore.Objects;
+using NUnit.Framework;
+
 namespace Serializer.UnitTest
 {
     public class BinarySerializationTest
     {
-        public BinarySerializationTest()
+        private DataContext data;
+        private DataRepository repo;
+        private Serializer serializer;
+        const string filename = "BinaryTest.json";
+
+        [SetUp]
+        public void Setup()
         {
+            serializer = new Serializer();
+            data = new DataContext();
+            repo = new DataRepository(new FillerForTest());
+            repo.Api.Fill(data);
+            if (!File.Exists(filename))
+            {
+                File.Create(filename).Dispose();
+            }
+        }
+
+        [Test]
+        public void SerializeAndDeserializeBinary()
+        {
+            //Serialize
+            serializer.SerializeItemBinary(filename, data);
+            FileInfo info = new FileInfo(filename);
+            Assert.IsTrue(info.Exists);
+            Assert.IsTrue(info.Length >= 100, $"File length: {info.Length}");
+
+            //Deserialize
+            DataContext dataFromBinary = serializer.DeserializeItemBinary(filename);
+            Assert.IsTrue(dataFromBinary.Equals(data));
+
         }
     }
 }
--- t1/Serializer.UnitTest/FillerForTest.cs
+++ t1/Serializer.UnitTest/FillerForTest.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using Bookstore;
+using Bookstore.Objects;
+
 namespace Serializer.UnitTest
 {
-    public class FillerForTest
+    public class FillerForTest : IDataFiller
     {
-        public FillerForTest()
+        public void Fill(DataContext context)
         {
+            Book book = new Book(0, "this is a book");
+            Status status = new Status(book, 39.99f);
+            Client client = new Client("Kamil", "Glik");
+            Client client2 = new Client("Ireneusz", "Jeleń");
+            Event purchase = new Purchase(client, status, DateTime.Now, false);
+            Event borrow = new Borrow(client2, status, DateTime.Now, DateTime.Now.AddMonths(1));
+            book.Events.Add(purchase);
+            book.Events.Add(borrow);
+            for (int i = 0; i < 10; i++)
+            {
+                context.Books.Add(i, book);
+                context.Clients.Add(client);
+                context.Events.Add(purchase);
+                context.Statuses.Add(status);
+            }
         }
     }
 }
