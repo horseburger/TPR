fc145be03ea0fddb42270e76ad3dc0327229eb53
ZygmuntJakub <216938@edu.p.lodz.pl>
2019-11-19 21:39:52 +0100

    merge

--- t1/Serializer.UnitTest/JSONSerializationTest.cs
+++ t1/Serializer.UnitTest/JSONSerializationTest.cs
@@ -41,6 +41,11 @@ namespace CustomSerializer.UnitTest
 
             //Deserialize
             DataContext dataFromJson = serializer.DeserializeItemJsonFromFile(filename);
+
+            for(int i = 0; i < dataFromJson.Events.Count;i++)
+            {
+                Console.WriteLine(dataFromJson.Events[i] + "\n" + data.Events[i]);
+            }
             Assert.IsTrue(dataFromJson.Equals(data));
         }
     }
