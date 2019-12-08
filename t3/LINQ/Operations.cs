f097391699c754b2c91df5ddfee0d4edab8d1318
horseburger <pralka.gniewu@gmail.com>
2019-12-08 14:59:33 +0100

    feat: implement all methods from stage 3

--- t3/LINQ/Operations.cs
+++ t3/LINQ/Operations.cs
@@ -1,10 +1,9 @@
 ﻿using LINQ;
-using System;
 using System.Collections.Generic;
 using System.Data.Linq.SqlClient;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Linq;
+
 
 namespace LINQProgram
 {
@@ -21,5 +20,75 @@ namespace LINQProgram
                 ).ToList();
         }
 
+        public static List<Product> GetProductsByVendorName(string vendorName)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from productVendor in productionDataContext.ProductVendors
+                where productVendor.Vendor.Name.Equals(vendorName)
+                select productVendor.Product
+                ).ToList();
+        }
+
+        public static List<string> GetProductNamesByVendorName(string vendorName)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from productVendor in productionDataContext.ProductVendors
+                where productVendor.Vendor.Name.Equals(vendorName)
+                select productVendor.Product.Name
+                ).ToList();
+        }
+
+        public static string GetProductVendorByProductName(string productName)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from productVendor in productionDataContext.ProductVendors
+                where productVendor.Product.Name.Equals(productName)
+                select productVendor.Vendor.Name
+                ).First();
+        }
+
+        public static List<Product> GetProductsWithNRecentReviews(int howManyReviews)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from product in productionDataContext.Product
+                where product.ProductReview.Count == howManyReviews
+                select product
+                ).ToList();
+        }
+
+        public static List<Product> GetNRecentlyReviewedProducts(int howManyProducts)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from pReview in productionDataContext.ProductReview
+                orderby pReview.ReviewDate descending
+                group pReview.Product by pReview.ProductID into p
+                select p.First()
+                ).Take(howManyProducts).ToList();
+        }
+
+        public static List<Product> GetNProductsFromCategory(string categoryName, int n)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (
+                from product in productionDataContext.Product
+                where product.ProductSubcategory.ProductCategory.Name.Equals(categoryName)
+                select product
+                ).Take(n).ToList();
+        }
+
+        public static int GetTotalStandardCostByCategory(ProductCategory category)
+        {
+            ProductionDataContext productionDataContext = new ProductionDataContext();
+            return (int)(
+                from product in productionDataContext.Product
+                where product.ProductSubcategory.ProductCategory.Name.Equals(category)
+                select product.StandardCost
+                ).ToList().Sum();
+        }
     }
 }
