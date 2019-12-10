147098d11db04ce93f96ff8fc9ee5bdc0abfc7fb
horseburger <pralka.gniewu@gmail.com>
2019-12-10 01:14:59 +0100

    feat: add NewProduct and NewProductContext

---
+++ t3/LINQ/NewProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class NewProduct : Product
+    {
+        public string diff { get; set; }
+
+        public NewProduct(Product p, string diff)
+        {
+            foreach(var prop in p.GetType().GetProperties())
+            {
+                if (prop.CanWrite)
+                {
+                    prop.SetValue(this, prop.GetValue(p));
+                }
+            }
+
+            this.diff = diff;
+        }
+    }
+}
---
+++ t3/LINQ/NewProductContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class NewProductContext
+    {
+        public List<NewProduct> products { get; private set; }
+
+        public NewProductContext(ProductionDataContext pDC)
+        {
+            this.products = pDC.Product.AsEnumerable().Select(product => new NewProduct(product, "a")).ToList();
+        }
+
+        public List<NewProduct> GetProductsByName(string name)
+        {
+            return (
+                from product in products
+                where product.Name.Equals(name)
+                select product
+                ).ToList();
+        }
+
+        public int GetStandardCostFromCategory(ProductCategory category)
+        {
+            return (int)(
+                from product in products
+                where product.ProductSubcategory.ProductCategory.Name.Equals(category.Name)
+                select product.StandardCost).ToList().Sum();
+        }
+
+        public List<NewProduct> GetNProductsByCategory(string category, int n)
+        {
+            return (
+                from product in products
+                where product.ProductSubcategory.ProductCategory.Name.Equals(category)
+                select product).Take(n).ToList();
+        }
+    }
+}
