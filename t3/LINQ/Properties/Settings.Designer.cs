8dc978aba7b9afd6c168d234cfea0c00602e10d3
horseburger <pralka.gniewu@gmail.com>
2020-01-13 21:33:25 +0100

    feat: init the WPF project; fix: change the connection string to match the new system

--- t3/LINQ/Properties/Settings.Designer.cs
+++ t3/LINQ/Properties/Settings.Designer.cs
@@ -55,5 +55,16 @@ namespace LINQ.Properties {
                 return ((string)(this["AdventureWorks2014ConnectionString2"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-MMOOIAF\\SQLEXPRESS;Initial Catalog=AdventureWorks2014;Integra" +
+            "ted Security=True")]
+        public string AdventureWorks2014ConnectionString3 {
+            get {
+                return ((string)(this["AdventureWorks2014ConnectionString3"]));
+            }
+        }
     }
 }
--- t3/ServiceUnitTest/APITests.cs
+++ t3/ServiceUnitTest/APITests.cs
@@ -13,7 +13,8 @@ namespace ServiceUnitTest
         [TestMethod]
         public void UpdateProductTest()
         {
-
+            API api = new API();
+            Assert.IsTrue(api.GetCount() != 0);
         }
     }
 }
