7f3ff9b28a61118330823ca819eccc485c28407c
horseburger <pralka.gniewu@gmail.com>
2020-01-14 08:11:27 -0800

    feat: add ValidateFloatNumberPositiveZero and add some more textboxes

---
+++ t3/WPF/Validators/ValidateFloatNumberPositiveZero.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WPF.Validators
+{
+    public class ValidateFloatNumberPositiveZero : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            bool flag = true;
+            if (value == null)
+            {
+                return new ValidationResult(false, "Value can't be empty");
+            } else
+            {
+                flag = decimal.TryParse((string)value, out decimal d);
+                if (!flag)
+                {
+                    return new ValidationResult(false, "Value must be a number");
+                }
+                if (d < 0)
+                {
+                    return new ValidationResult(false, "Value can't be negative");
+                }
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
